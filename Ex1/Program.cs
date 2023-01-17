365b45873235679f7977fbecacc545359474ef07
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-17 21:13:35 +0300

    add CSharpSeminar2

---
+++ Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿// SНапишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+
+// 25->5
+int num = new.Random.Next[10,100];
+System.Console.WriteLine(num);
+
+int firstNum = num/ 10;
+int secondNum = num % 10;
+
+if(firstNum > secondNum)
+{
+    System.Console.WriteLine(secondNum);
+}
+else
+{
+    System.Console.WriteLine(firstNum);
+}
